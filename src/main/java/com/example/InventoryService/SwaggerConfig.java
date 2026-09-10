54f0d5534a8f593a2f9faed464ecfa6231e40dc3
Rahul Havale <rahulhavale256@gmail.com>
2026-09-10 16:16:10 +0530

    new laptop

--- src/main/java/com/example/InventoryService/SwaggerConfig.java
+++ src/main/java/com/example/InventoryService/SwaggerConfig.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -25,6 +27,11 @@ public class SwaggerConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8086")
+                                .description("API Gateway")
+                ))
                 .externalDocs(new ExternalDocumentation()
                         .description("Project Documentation")
                         .url("https://springdoc.org"));
--- src/main/java/com/example/InventoryService/SwggerConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.InventoryService;
-
-public class SwggerConfig {
-}
